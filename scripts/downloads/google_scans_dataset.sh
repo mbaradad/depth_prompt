46c5d96a6a25cb0e07f77bfb0914afed4b436cde
Manel Baradad Jurjo <mbaradad@mit.edu>
2023-09-07 10:52:32 -0400

    Repaired download links

--- scripts/downloads/google_scans_dataset.sh
+++ scripts/downloads/google_scans_dataset.sh
@@ -2,8 +2,9 @@
 
 #!/bin/bash
 mkdir -p datasets
-mkdir -p datasets/google_scans
 
 wget -O datasets/google_scans.zip http://data.csail.mit.edu/synthetic_training/background_prompting/datasets/google_scans.zip
+unzip datasets/google_scans.zip -d datasets/google_scans
+rm datasets/google_scans.zip
 
 
--- scripts/downloads/nerfies_dataset.sh
+++ scripts/downloads/nerfies_dataset.sh
@@ -1,25 +1,10 @@
 #/bin/bash
 
 #!/bin/bash
-mkdir -p encoders
+mkdir -p datasets
 
-encoders=('single_background_fft_midas_conv.pth.tar' \
-          'single_background_fft_leres_resnet50.pth.tar' \
-          'single_background_fft_omnidata.pth.tar' \
-          'single_background_fft_dpt.pth.tar' \
-
-          'hypernet_fft_midas_conv.pth.tar' \
-          'hypernet_fft_leres_resnet50.pth.tar' \
-          'hypernet_fft_omnidata.pth.tar' \
-          'hypernet_fft_dpt.pth.tar' \
-           )
-
-for ENCODER in ${encoders[@]}
-do
-    echo "Downloading $ENCODER"
-    wget -O encoders/$ENCODER http://data.csail.mit.edu/synthetic_training/background_prompting/models/$ENCODER
-done
-
-
+wget -O datasets/nerfies.zip http://data.csail.mit.edu/synthetic_training/background_prompting/datasets/nerfies.zip
+unzip datasets/nerfies.zip -d datasets/nerfies
+rm datasets/nerfies.zip
 
 
